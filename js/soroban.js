c82e8666c64b3776cc08696249fa350397e69f7f
tripu <tripu@tripu.info>
2013-12-20 01:29:00 +0100

    First functional iteration (prototype).

--- js/soroban.js
+++ js/soroban.js
@@ -3,5 +3,56 @@
  * Soroban JS
  */
 
+(function(globals){
+
+'use strict';
+
+var CONST = {'MIN': 2,
+             'MAX': 11};
+
+var operand1;
+var operand2;
+var result;
+
+$(document).ready(function() {
+
+    // $('#result').bind('enterKey', checker);
+
+    $('#result').keyup(function(event) {
+
+        if (13 === event.keyCode) {
+            check(parseInt($(this)[0].value));
+        }
+
+    });
+
+    mainLoop();
+
+});
+
+function mainLoop() {
+
+    globals.operand1 = Math.floor(CONST.MIN + Math.random() * (CONST.MAX - CONST.MIN + 1));
+    globals.operand2 = Math.floor(CONST.MIN + Math.random() * (CONST.MAX - CONST.MIN + 1));
+    globals.result = parseInt(globals.operand1 * globals.operand2);
+
+    $('#operand1').html(globals.operand1);
+    $('#operand2').html(globals.operand2);
+    $('#result')
+
+}
+
+function check(answer) {
+
+    if (answer === globals.result) {
+        window.alert('OK!');
+    } else {
+        window.alert('Nope. It\'s ' + globals.result + '.');
+    }
+
+}
+
+}(this));
+
 // EOF
 
