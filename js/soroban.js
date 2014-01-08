83a565bfb0b1d2189b6332f37a3bb19a1ca37889
tripu <tripu@tripu.info>
2014-01-08 23:32:41 +0100

    Multiplication tables pop-up is functional.

--- js/soroban.js
+++ js/soroban.js
@@ -16,6 +16,8 @@ var totalOps = 10;
 var operand1;
 var operand2;
 var result;
+var multTableMin;
+var multTableMax;
 
 $(document).ready(function() {
 
@@ -25,6 +27,8 @@ $(document).ready(function() {
         function(){ $(this).addClass('fa-spin') },
         function(){ $(this).removeClass('fa-spin') }
     );
+
+    $('#tables').on('shown.bs.modal', buildMultiplicationTables);
 
     $('#result').keyup(function(event) {
 
@@ -67,6 +71,57 @@ function check(answer) {
         $('#result').prop('disabled', true);
     } else {
         mainLoop();
+    }
+
+}
+
+function buildMultiplicationTables() {
+
+    if (undefined === multTableMin || undefined === multTableMax || multTableMin !== CONST.MIN || multTableMax !== CONST.MAX) {
+        // Build an updated table.
+        var i;
+        var j;
+
+        // 1. Header:
+        var header = $('#tableOfTables > thead > tr');
+        // var headerContent = '<td class="symbol">&times;</td>';
+        var headerContent = '<td/>';
+
+        header.empty();
+
+        for (i = Math.max(2, CONST.MIN); i <= CONST.MAX; i ++) {
+            headerContent += '<td>' + i + '</td>';
+        }
+
+        header.html(headerContent);
+
+        // 2. Rows:
+        var rows = $('#tableOfTables > tbody');
+        var rowsContent = '';
+
+        rows.empty();
+
+        for (i = Math.max(2, CONST.MIN); i <= CONST.MAX; i ++) {
+            rowsContent += '<tr><td class="axis">' + i + '</td>';
+
+            for (j = Math.max(2, CONST.MIN); j < i; j ++) {
+                rowsContent += '<td/>';
+            }
+
+            rowsContent += '<td class="square' + (Math.max(i, j) > 10 ? ' extended' : '') + '">' + (i * j++) + '</td>';
+
+            for (; j <= CONST.MAX; j ++) {
+                rowsContent += '<td' + (Math.max(i, j) > 10 ? ' class="extended"' : '') + '>' + (i * j) + '</td>';
+            }
+
+            rowsContent += '</tr>';
+        }
+
+        rows.html(rowsContent);
+
+        // 3. Save boundaries, for checking in the future:
+        multTableMin = CONST.MIN;
+        multTableMax = CONST.MAX;
     }
 
 }
